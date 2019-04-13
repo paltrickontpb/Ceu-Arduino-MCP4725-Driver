bee50f688a8fb8f1bb88fd738dd8c42d686e6982
paltrickontpb <paltrickontpb@gmail.com>
2019-04-14 02:33:16 +0530

    Added base library and header

---
+++ MCP4725.h
@@ -0,0 +1,9 @@
+#ifndef MCP4725_h
+#define MCP4725_h
+
+#define MCP4725_ADDRESS                 (0x00)
+
+#define MCP4725_CMD_WRITEDAC            (0x40)  
+#define MCP4725_CMD_WRITEDACEEPROM      (0x60) 
+
+#endif
